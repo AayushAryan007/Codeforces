07854acd6d9caf0652e876206dca05e4cce23b5e
Ayush <unitedestates.ayush10@gmail.com>
2024-01-06 22:49:40 +0530

    cses dp + cf hello 2024

---
+++ Hello 2024 C. Grouping Increases.cpp
@@ -0,0 +1,239 @@
+// Problem: C. Grouping Increases
+// Contest: Codeforces - Hello 2024
+// URL: https://codeforces.com/contest/1919/problem/C
+// Memory Limit: 256 MB
+// Time Limit: 1000 ms
+// 
+// Powered by CP Editor (https://cpeditor.org)
+
+#include <bits/stdc++.h> 
+using namespace std;  
+#define endl "\n"
+#define lo long long
+#define dob long double
+#define ll long long int
+#define mod 1000000007
+#define pb(n) push_back(n)
+#define mp(a,b) make_pair(a,b)
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define fr(i, x, n) for (int i = x; i < n; i++)
+#define nfr(i, x, n) for (int i = x; i > n; i--)
+#define sp(x) setprecision(x)
+#define vi vector<int>
+#define vpp vector<pair<int,int>>
+#define vs vector<string>
+#define vll vector<long long>
+#define in(v)  for(auto &item : v) cin>>item;
+#define inp(v)  for(auto &item : v) cin>>item.ff>>item.ss;
+#define ub(x) upper_bound(x)
+
+#define ss second
+#define ff first
+//count set bits\|/
+#define bp(a) __builtin_popcount(a)
+
+int binpow(int a, int b)
+{
+  if(b==0)
+  return 1;
+  int res=binpow(a,b/2);
+
+  if(b%2)
+  return res*res*a;
+  else
+  return res*res;
+}
+int gcd(int a, int b) { return b == 0 ? a : gcd(b, a % b); }
+int lcm(int a, int b) { return (a / gcd(a, b)) * b; }
+int lowpowerof2(int n) { return 1LL << (int)log2(n); }
+int highpowerof2(int n) { return 1LL << (int)ceil(log2(n)); }
+bool ispowerof2(int n) { return (n != 0) && ((n & (n - 1)) == 0); }
+
+// int gcd(int a, int b)
+// {
+    // // Everything divides 0
+    // if (a == 0)
+       // return b;
+    // if (b == 0)
+       // return a;
+//   
+    // // base case
+    // if (a == b)
+        // return a;
+//   
+    // // a is greater
+    // if (a > b)
+        // return gcd(a-b, b);
+    // return gcd(a, b-a);
+// }
+
+//prime ll
+bool isprime(long long int n)  //to check prime
+{
+    for(int i=2; i*i<=n; i++)
+    {
+        if(n%i==0)return false;
+    }
+    return true;
+}
+//prime int
+bool isPrime(int n) 
+{ 
+    // Corner cases 
+    if (n <= 1) 
+        return false; 
+    if (n <= 3) 
+        return true; 
+  
+    // This is checked so that we can skip 
+    // middle five numbers in below loop 
+    if (n % 2 == 0 || n % 3 == 0) 
+        return false; 
+  
+    for (int i = 5; i * i <= n; i = i + 6) 
+        if (n % i == 0 || n % (i + 2) == 0) 
+            return false; 
+  
+    return true; 
+} 
+
+
+int LIS(vector<int> &a)//returns longest non decreasing subsequence
+{
+  //lnds[i]=element at which an increasng subsequence of length 'i' ends
+
+  vector<int> lis;
+  for(auto &x:a)
+  {
+    auto it=upper_bound(lis.begin(),lis.end(), x); //upper_bound for non-decreasing
+    if(it==lis.end())
+    lis.push_back(x);
+    else
+    *it=x;
+  }
+
+  return lis.size();
+}
+ 
+ //iota(v.begin(),v.end(),1); assign 1-n incrementation in vector
+ void reverse(int arr[], int index, int count)
+{
+    if (index < count)
+    {
+        swap(arr[index], arr[count]);
+        reverse(arr, index + 1, count - 1);
+    }
+}
+
+//print binary of int ,10 bits
+void printBinary(int num){
+	for(int i=10;i>=0;i--){
+		cout<<((num>>i)&1);
+	}
+	cout<<endl;
+}
+
+////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////
+
+// void solve()
+// {
+    // int n;
+    // cin>>n;
+//     
+    // int count=0;
+    // vi v(n);
+    // int flag=0;
+   // cin>>v[0];
+//     
+    // fr(i,1,n){
+    	  // cin>>v[i];
+    	  // if(v[i]>v[i-1])
+    	  // {count++; flag=1;}
+//     	  
+    // } 
+//     
+    // cout<<(flag? count-1: 0)<<endl;
+// }
+// 
+
+// void solve(){
+	// int n;
+	// cin>>n;
+	// vi v(n),a(n),b(n);
+	// cin>>v[0];
+	// a.pb(v[0]);
+	// fr(i,1,n){
+		// cin>>v[i];
+		// if(v[i]<v[i-1])
+		// a.pb(v[i]);
+		// else b.pb(v[i]);
+	// }
+	// int ans=0;
+	// fr(i,1,b.size()){
+		// if(b[i]>b[i-1]) ans++;
+	// }
+	// cout<<max(0,ans-1)<<endl;
+// }
+void solve(){
+	// int n;
+	// cin>>n;
+	// vi v(n),a(n),b(n);
+	// cin>>v[0];
+// 
+	// fr(i,1,n){
+		// cin>>v[i];
+     // } 
+//      
+//      
+     // int ans=INT_MAX;
+//      
+     // fr(i,1,n){
+//      	
+     // }
+     
+     ll n;
+     cin>>n;
+     vll v(n);
+     ll a=INT_MAX,b=INT_MAX;
+     int count=0;
+     fr(i,0,n){
+     	cin>>v[i];
+     	
+     	if(a>b) {
+     		int t=a;
+     		a=b;b=t;
+     		
+     	}
+     	if (a>=v[i]) {
+                a = v[i];
+            } else if (b>=v[i]) {
+                b = v[i];
+            } else {
+                a = v[i];
+                count++;
+
+     	}
+     	
+     }
+     cout<<count<<endl;
+     		
+}		
+		
+		
+int main()  
+{
+    ios_base::sync_with_stdio(false);  
+    cin.tie(NULL); cout.tie(NULL); // flushes cout  
+    
+    //start writing below
+    int t;
+    cin>>t;
+    while(t--)
+    {
+       solve(); 
+    }
+ 
+    return 0;  
+}
