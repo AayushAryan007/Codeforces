ebc45e88208b69dbc42a1414eba38c19981a4f01
Ayush <unitedestates.ayush10@gmail.com>
2024-04-13 22:57:34 +0530

    cf virtual

---
+++ 938 B. Progressive Square.cpp
@@ -0,0 +1,252 @@
+// Problem: B. Progressive Square
+// Contest: Codeforces - Codeforces Round 938 (Div. 3)
+// URL: https://codeforces.com/contest/1955/problem/B
+// Memory Limit: 256 MB
+// Time Limit: 2000 ms
+// 
+// Powered by CP Editor (https://cpeditor.org)
+
+#include <bits/stdc++.h> 
+using namespace std;  
+#define endl "\n"
+#define lo long long
+#define dob long double
+//#define ll long long int
+#define int long long 
+#define mod 1000000007
+#define pb(n) push_back(n)
+#define mp(a,b) make_pair(a,b)
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define fr(i, x, n) for (int i = x; i < n; i++)
+#define nfr(i, x, n) for (int i = x; i > n; i--)
+#define sp(x) setprecision(x)
+#define vi vector<int>
+#define vpp vector<pair<int,int>>
+#define vs vector<string>
+#define vll vector<long long>
+#define in(v)  for(auto &item : v) cin>>item;
+#define inp(v)  for(auto &item : v) cin>>item.ff>>item.ss;
+#define ub(x) upper_bound(x)
+// #define pq priority_queue<int, std::vector<int>, std::greater<int>>
+#define ss second
+#define ff first
+//count set bits\|/
+#define bp(a) __builtin_popcount(a)
+
+int binpow(int a, int b)
+{
+  if(b==0)
+  return 1;
+  int res=binpow(a,b/2);
+
+  if(b%2)
+  return res*res*a;
+  else
+  return res*res;
+}
+// int gcd(int a, int b) { return b == 0 ? a : gcd(b, a % b); }
+// int lcm(int a, int b) { return (a / gcd(a, b)) * b; }
+// int lowpowerof2(int n) { return 1LL << (int)log2(n); }
+// int highpowerof2(int n) { return 1LL << (int)ceil(log2(n)); }
+// bool ispowerof2(int n) { return (n != 0) && ((n & (n - 1)) == 0); }
+// 
+// int gcd(int a, int b)
+// {
+    // // Everything divides 0
+    // if (a == 0)
+       // return b;
+    // if (b == 0)
+       // return a;
+//   
+    // // base case
+    // if (a == b)
+        // return a;
+//   
+    // // a is greater
+    // if (a > b)
+        // return gcd(a-b, b);
+    // return gcd(a, b-a);
+// }
+
+//prime ll
+bool isprime(int n)  //to check prime
+{
+    for(int i=2; i*i<=n; i++)
+    {
+        if(n%i==0)return false;
+    }
+    return true;
+}
+//prime int
+bool isPrime(int n) 
+{ 
+    // Corner cases 
+    if (n <= 1) 
+        return false; 
+    if (n <= 3) 
+        return true; 
+  
+    // This is checked so that we can skip 
+    // middle five numbers in below loop 
+    if (n % 2 == 0 || n % 3 == 0) 
+        return false; 
+  
+    for (int i = 5; i * i <= n; i = i + 6) 
+        if (n % i == 0 || n % (i + 2) == 0) 
+            return false; 
+  
+    return true; 
+} 
+
+
+int LIS(vector<int> &a)//returns longest non decreasing subsequence
+{
+  //lnds[i]=element at which an increasng subsequence of length 'i' ends
+
+  vector<int> lis;
+  for(auto &x:a)
+  {
+    auto it=upper_bound(lis.begin(),lis.end(), x); //upper_bound for non-decreasing
+    if(it==lis.end())
+    lis.push_back(x);
+    else
+    *it=x;
+  }
+
+  return lis.size();
+}
+ 
+ //iota(v.begin(),v.end(),1); assign 1-n incrementation in vector
+ void reverse(int arr[], int index, int count)
+{
+    if (index < count)
+    {
+        swap(arr[index], arr[count]);
+        reverse(arr, index + 1, count - 1);
+    }
+}
+
+//print binary of int ,10 bits
+void printBinary(int num){
+	for(int i=10;i>=0;i--){
+		cout<<((num>>i)&1);
+	}
+	cout<<endl;
+}
+// 
+// long long powi(long long base, long long exponent) {
+    // long long result = 1;
+    // while (exponent > 0) {
+        // if (exponent % 2 == 1) {
+            // result *= base;
+        // }
+        // base *= base;
+        // exponent /= 2;
+    // }
+    // return result;
+// }
+
+//i power of number function
+int powi(int base, int exponent) {
+     int result = 1;
+    while (exponent > 0) {
+        if (exponent % 2 == 1) {
+            result *= base;
+        }
+        base *= base;
+        exponent /= 2;
+    }
+    return result;
+}
+
+int sumvec(vector<int> & v){
+	int sum=0;
+	fr(i,0,v.size()){
+		sum+=v[i];
+	}
+	return sum;
+}
+
+// int N=1e7+5;
+// vector<ll> spf(N+1,1);
+// 
+// void sieve(){
+	// ll i,j;
+	// for(i=2;i<N;i++){
+		// if(spf[i]==1){
+			// spf[i]=i;
+			// for(j=i*i;j<=N;j+=i){
+				// if(spf[j]==1){
+					// spf[j]=i;
+				// }
+			// }
+		// }
+	// }
+// }
+
+// #ifndef Aayush_Ryan
+// #include "C:\Users\Ayush\Documents\Codeforces\handle_debug.hpp" 
+// #define debug(...) cout<<'[';debug_print(#__VA_ARGS__, __VA_ARGS__);cout<<"]\n";
+// #else
+// #define debug(...)
+// #endif
+ 
+////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////
+
+void solve() {
+    int n, c, d;
+    cin >> n >> c >> d;
+    int mini = INT_MAX;
+    int N = n * n;
+    vector<int> arr(N);
+    fr(i, 0, N) {
+        cin >> arr[i];
+        mini = min(mini, arr[i]);
+    }
+
+    int brr[n][n];
+
+    brr[0][0] = mini;
+
+    for(int i = 1; i < n; i++) {
+        brr[0][i] = brr[0][i - 1] + c;
+        brr[i][0] = brr[i - 1][0] + d;
+    }
+
+    for(int i = 1; i < n; i++) {
+        for(int j = 1; j < n; j++) {
+            brr[i][j] = min(brr[i - 1][j] + d, brr[i][j - 1] + c);
+        }
+    }
+
+    vector<int> BRR(n * n);
+    int k = 0;
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            BRR[k++] = brr[i][j];
+        }
+    }
+
+    sort(all(BRR));
+    sort(all(arr));
+
+    // Check if arr and BRR are the same
+    if(arr == BRR) {
+        cout << "YES" << endl;
+    } else {
+        cout << "NO" << endl;
+    }
+}
+
+signed main() {
+    int t;
+    cin >> t;
+
+    while(t--) {
+        solve();
+    }
+
+    return 0;
+}
